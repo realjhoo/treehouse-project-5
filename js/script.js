83467e180f12f7ffdac62e447baaac587e211641
Jerry L Hoover <jerry.hoover@ymail.com>
2019-04-21 21:11:03 -0500

    Peer Review Commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// Hey, input box... what's up?
+document.getElementById("searchInput").addEventListener("keyup", liveSearchFunction);
+
+function liveSearchFunction() {
+   const anchor = document.querySelectorAll(".photo-gallery a");
+   let tempSearchString = document.getElementById("searchInput");
+   let searchString = tempSearchString.value.toLowerCase();
+
+   for (var i = 0; i < anchor.length; i++) {
+      // loop thru data-titles
+      let caption = anchor[i].getAttribute("data-title");
+      // LCASE the data-title
+      let lcaseCaption = caption.toLowerCase()
+      // Compare the strings
+      let isIn = lcaseCaption.includes(searchString);
+
+      if (isIn) {
+         // set the display
+         anchor[i].style.display = "grid"
+      } else {
+         // hide it
+         anchor[i].style.display = "none"
+      }
+   }
+
+}
